3f0e3a281604fdbe8d7308da2c7165550c84c53b
Daksha Rongala <rongala.daksha11@gmail.com>
2024-09-08 20:34:44 +0530

    Background Image

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import background from "../Images/backgroundImage.png";
 import SignUp from './sign_up_page';
 
-function App() {
-  return (
-    <SignUp />
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SignUp />
+      </div>
+    );
+  }
 }
 
 export default App;
