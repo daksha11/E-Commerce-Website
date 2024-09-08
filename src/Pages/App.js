94eb5a85f586a6e1368c173eea5f0fe4ff529244
Daksha Rongala <rongala.daksha11@gmail.com>
2024-09-08 19:49:09 +0530

    Sign Up Page

---
+++ src/Pages/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import SignUp from './sign_up_page';
+
+function App() {
+  return (
+    <SignUp />
+  );
+}
+
+export default App;
---
+++ src/Pages/sign_up_page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpComponent from "../Components/sign_up_component";
+
+const SignUp = () => {
+    return (
+        <div style={{maxWidth: "fit-content", margin: "auto", marginTop: "9.5%"}}>
+            <SignUpComponent />
+        </div>
+    );
+}
+
+export default SignUp;
